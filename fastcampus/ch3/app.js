1dd53548d1ec8371c224ac723f99b6c4f6cbb34e
shinichi <dhcho1034@gmail.com>
2020-10-12 21:18:20 +0900

    nunjucks macro

--- fastcampus/ch3/app.js
+++ fastcampus/ch3/app.js
@@ -19,17 +19,33 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use('/uploads', express.static('uploads'));
+
+app.use((req, res, next) => {
+  app.locals.isLogin = false;
+  app.locals.req_path = req.path;
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("express start");
 });
 
-const vipMiddleware = (req, res, next) => {
+const vipMiddleware = (_, res, next) => {
   console.log("최우선 미들웨어");
   next();
 };
 
 app.use("/admin", vipMiddleware, admin);
 app.use("/contacts", contacts);
+
+app.use((req, res, _) => {
+  res.status(400).render('common/404.html');
+});
+
+app.use((req, res, _) => {
+  res.status(500).render('common/500.html');
+});
 
 app.listen(port, () => {
   console.log("Express listening on port", port);
