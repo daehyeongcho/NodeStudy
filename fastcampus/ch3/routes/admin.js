a2c4e283bbf921a2217b880a5e83866747b1b02c
dhcho1034 <dhcho1034@gmail.com>
2020-10-12 16:50:11 +0900

    bodyparser

--- fastcampus/ch3/routes/admin.js
+++ fastcampus/ch3/routes/admin.js
@@ -2,7 +2,24 @@ const express = require("express");
 const router = express.Router();
 const nunjucks = require("nunjucks");
 
-router.get("/", (req, res) => {
+const testMiddleware = (req, res, next) => {
+  console.log("첫번째 미들웨어");
+  next();
+};
+const testMiddleware2 = (req, res, next) => {
+  console.log("두번째 미들웨어");
+  next();
+};
+
+// const loginRequired = (req, res, next) => {
+//   if (로그인이되어있지않으면) {
+//     res.redirect(로그인창으로);
+//   } else {
+//     next();
+//   }
+// };
+
+router.get("/", testMiddleware, testMiddleware2, (req, res) => {
   res.send("admin 이후 url");
 });
 
@@ -12,6 +29,10 @@ router.get("/products", (req, res) => {
     message: "<h1>태그가 출력됩니다.<h1>",
     online: "express",
   });
+});
+
+router.get("/products/write", (req, res) => {
+  res.render("admin/write.html");
 });
 
 module.exports = router;
