07973e4c0aeaa6b4a53268e6bc166aacb322a2e2
dhcho1034 <dhcho1034@gmail.com>
2020-10-15 17:34:28 +0900

    모델 작성

--- fastcampus/sequelize-crud-ex/app.js
+++ fastcampus/sequelize-crud-ex/app.js
@@ -29,6 +29,7 @@ class App {
       })
       .then(() => {
         console.log("DB Sync complete.");
+        return db.sequelize.sync();
       })
       .catch((err) => {
         console.error("Unable to connect to the database:", err);
